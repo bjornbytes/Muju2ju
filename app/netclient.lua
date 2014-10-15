69218be805c732f70291e1fa16e3b95456a69ece
bjorn <tie.372@gmail.com>
2014-10-15 13:35:37 -0600

    Juju network bugfix;

--- app/netclient.lua
+++ app/netclient.lua
@@ -131,8 +131,10 @@ NetClient.messages.jujuCreate = {
   receive = function(self, event)
     ctx.event:emit('jujuCreate', event.data)
     local juju = ctx.jujus.objects[event.data.id]
-    for i = 1, (self.server:round_trip_time() / 1000) / tickRate do
-      juju:update()
+    if juju then
+      for i = 1, (self.server:round_trip_time() / 1000) / tickRate do
+        juju:update()
+      end
     end
   end
 }

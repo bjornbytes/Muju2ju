8267b21e97c775413086d391f898f4284884b102
bjorn <tie.372@gmail.com>
2014-10-15 13:52:44 -0600

    HudTimer;

--- app/hud/hudtimer.lua
+++ app/hud/hudtimer.lua
@@ -6,7 +6,6 @@ function HudTimer:draw()
   if ctx.ded then return end
 
   local u, v = ctx.hud.u, ctx.hud.v
-  local font = ctx.hud.boldFont
 
   local total = ctx.timer * tickRate
   local seconds = math.floor(total % 60)
@@ -16,7 +15,7 @@ function HudTimer:draw()
 
   local str = minutes .. ':' .. seconds
   g.setColor(255, 255, 255)
-  g.setFont(font)
-  g.print(str, ctx.view.frame.width - 25 - font:getWidth(str), 25)
+  g.setFont('inglobalb', .03 * v)
+  g.print(str, u - (.04 * v) - g.getFont():getWidth(str), v * .04)
 end
 

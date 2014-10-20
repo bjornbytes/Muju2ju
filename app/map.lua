d32b23741dca978d2755241f93a378af17c3a615
bjorn <tie.372@gmail.com>
2014-10-20 10:26:12 -0500

    Start map graphics;

--- app/map.lua
+++ app/map.lua
@@ -1,23 +1,32 @@
 Map = class()
 
 Map.width, Map.height = 1500, 600
+Map.depth = -100
 
-local function drawBackground(self)
-  local g = love.graphics
-  local p = ctx.players:get(ctx.id)
+local backgroundData = {
+  background = {x = 0, y = 0, depth = 10},
+  grassFront1 = {x = 0, y = 837, depth = 0},
+  grassFront2 = {x = 1909, y = 827, depth = 0}
+}
 
-  if not p then return end
+local function drawBackground(key)
+  return function()
+    local p = ctx.players:get(ctx.id)
+    if not p then return end
 
-  local scale = ctx.map.height / data.media.graphics.bg:getHeight()
-  local alpha = ctx.map.spiritAlpha * 255
-  alpha = math.lerp(alpha, (1 - (p.healthDisplay / p.maxHealth)) * 255, .5)
-  for i = 0, ctx.map.width, data.media.graphics.bg:getWidth() * scale do
+    local g = love.graphics
+    local image = data.media.graphics.map[key]
+    local spirit = data.media.graphics.map[key .. 'Spirit']
+    local map = ctx.map
+    local scale = ctx.map.height / data.media.graphics.map.background:getHeight()
+    local alpha = map.spiritAlpha * 255
+    alpha = math.lerp(alpha, (1 - (p.healthDisplay / p.maxHealth)) * 255, .5)
+
     g.setColor(255, 255, 255)
-    g.draw(data.media.graphics.bg, i, 0, 0, scale, scale)
+    g.draw(image, backgroundData[key].x * scale, backgroundData[key].y * scale, 0, scale, scale)
     g.setColor(255, 255, 255, alpha)
-    g.draw(data.media.graphics.bgSpirit, i, 0, 0, scale, scale)
+    g.draw(spirit, backgroundData[key].x * scale, backgroundData[key].y * scale, 0, scale, scale)
   end
-
 end
 
 function Map:init()
@@ -25,22 +34,27 @@ function Map:init()
 
   self.spiritAlpha = 0
 
-  self.background = {
-    depth = 5,
-    draw = drawBackground
-  }
-
   if ctx.view then
     ctx.view.xmax = self.width
     ctx.view.ymax = self.height
     ctx.view.x = self.width / 2 - ctx.view.width / 2
   end
+  
+  ctx.event:emit('view.register', {object = self})
 
-  ctx.event:emit('view.register', {object = self.background})
+  local backgrounds = {'background', 'grassFront1', 'grassFront2'}
+  table.each(backgrounds, function(background)
+    local object = {
+      depth = backgroundData[background].depth,
+      draw = drawBackground(background)
+    }
+
+    ctx.event:emit('view.register', {object = object})
+  end)
 end
 
-function Map:update()
+function Map:draw()
   if ctx.id then
-    self.spiritAlpha = math.lerp(self.spiritAlpha, ctx.players:get(ctx.id).dead and 1 or 0, .6 * tickRate)
+    self.spiritAlpha = math.lerp(self.spiritAlpha, ctx.players:get(ctx.id).dead and 1 or 0, 6 * delta)
   end
 end

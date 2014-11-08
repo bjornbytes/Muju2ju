e318cd683bab09dcd7bc4a93a6f5a4b0fc2aabcd
bjorn <tie.372@gmail.com>
2014-11-07 19:46:31 -0800

    Finish summon bar;

--- app/hud/hudhealth.lua
+++ app/hud/hudhealth.lua
@@ -2,8 +2,12 @@ HudHealth = class()
 
 local g = love.graphics
 
+local green = {50, 230, 50}
+local red = {255, 0, 0}
+local purple = {200, 80, 255}
+local orange = {255, 185, 40}
+
 local function bar(x, y, hard, soft, color, width, height)
-	thickness = thickness or 2
   x, y = ctx.view:screenPoint(x, y)
   width = width * ctx.view.scale
 
@@ -22,8 +26,10 @@ local function bar(x, y, hard, soft, color, width, height)
   g.setColor(color[1], color[2], color[3], 200)
   g.draw(data.media.graphics.healthbarBar, xx, yy, 0, hard * math.round(width - 6 * scale), scale)
 
-  g.setColor(color[1], color[2], color[3], 100)
-  g.draw(data.media.graphics.healthbarBar, xx, yy, 0, soft * math.round(width - 6 * scale), scale)
+  if soft then
+    g.setColor(color[1], color[2], color[3], 100)
+    g.draw(data.media.graphics.healthbarBar, xx, yy, 0, soft * math.round(width - 6 * scale), scale)
+  end
 
   g.setBlendMode('additive')
   g.setColor(255, 255, 255, 180)
@@ -40,30 +46,19 @@ end
 function HudHealth:draw()
   if ctx.net.state == 'ending' then return end
 
-  local green = {50, 230, 50}
-  local red = {255, 0, 0}
-  local purple = {200, 80, 255}
-
   local p = ctx.players:get(ctx.id)
   local vx, vy = math.lerp(ctx.view.prevx, ctx.view.x, tickDelta / tickRate), math.lerp(ctx.view.prevy, ctx.view.y, tickDelta / tickRate)
 
   ctx.players:each(function(player)
     local x, y, hard, soft = player:getHealthbar()
 
-    -- Draw summon bar
-
+    -- Summon bar
     local t = math.lerp(player.summonTweenPrevTime, player.summonTweenTime, tickDelta / tickRate)
     player.summonTween:set(t)
     local summonFactor = player.summonFactor.value
-
     local summonTimer = math.lerp(player.summonPrevTimer, player.summonTimer, tickDelta / tickRate)
-
-    local bx, by = ctx.view:screenPoint(x, y)
     local width, height = math.max(120 * summonFactor, 0), math.max(10 * summonFactor, 0)
-    g.setColor(0, 0, 0, 200 * math.clamp(summonFactor, 0, 1))
-    g.rectangle('fill', bx - width / 2, by - 15 - 25 * summonFactor, width, height)
-    g.setColor(255, 255, 0, 255 * math.clamp(summonFactor, 0, 1))
-    g.rectangle('fill', bx - width / 2 + 1, by - 15 - 25 * summonFactor + 1, (summonTimer / 2) * (width - 2), height - 2)
+    bar(x, y - 15 - 25 * summonFactor, summonTimer / 2, nil, orange, width, height)
 
     local color = (p and player.team == p.team) and green or red
     bar(x, y - 15, hard, soft, color, 80, 3)

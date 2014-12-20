3fa7c1d21ac84866d7fdef25a1b3d33c8feea56d
bjorn <tie.372@gmail.com>
2014-12-19 23:22:57 -0700

    Unkill chat;

--- app/hud/hudchat.lua
+++ app/hud/hudchat.lua
@@ -116,7 +116,6 @@ function HudChat:resize()
 end
 
 function HudChat:refresh()
-  do return end
   if #self.log == 0 then return end
   local u, v = ctx.hud.u, ctx.hud.v
   local width = u * .25 - 4

b852ddcc21aa1bb2f0d959099e777c8462ebf077
bjorn <tie.372@gmail.com>
2014-12-20 15:50:03 -0700

    Work on burst;

--- data/ability/bruju/burst.lua
+++ data/ability/bruju/burst.lua
@@ -12,14 +12,24 @@ Burst.description = 'Burst things'
 -- Data
 ----------------
 Burst.passive = true
+Burst.damage = 50
+Burst.range = 100
 
 
 ----------------
 -- Behavior
 ----------------
 function Burst:die()
-  local damage = 50 + 
-  ctx.spells:add(data.spell.bruju.burst, {owner = self})
+  local damage = self.damage * (self:hasUpgrade('essenceflame') and 1.5 or 1)
+  local range = self.range * (self:hasUpgrade('essenceflame') and 2 or 1)
+  local heal = self:hasUpgrade('sanctuary') and self.damage or 0
+
+  ctx.spells:add(data.spell.bruju.burst, {
+    owner = self,
+    damage = damage,
+    range = range,
+    heal = heal
+  })
 end
 
 
@@ -27,10 +37,12 @@ end
 -- Upgrades
 ----------------
 local EssenceFlame = {}
+EssenceFlame.code = 'essenceflame'
 EssenceFlame.name = 'Essence Flame'
 EssenceFlame.description = 'Burst deals 50% increased damage and the radius is increased by 100%.'
 
 local Sanctuary = {}
+Sanctuary.code = 'sanctuary'
 Sanctuary.name = 'Sanctuary'
 Sanctuary.description = 'Burst heals allies in the area of effect for 15% of their maximum health.'
 

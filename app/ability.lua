f66780a964a3efdf16e8417ddb4008a05c02da16
Trey Cordova <treycordova@hotmail.com>
2014-12-21 13:28:32 -0800

    Added automatic cooldown handling in Ability;

--- app/ability.lua
+++ app/ability.lua
@@ -1,5 +1,9 @@
 Ability = class()
 
+function Ability:init()
+  self.timer = self.cooldown or 0
+end
+
 function Ability:getUnitDirection()
   return self.unit.flipped and 1 or -1 
 end
@@ -7,4 +11,18 @@ end
 function Ability:hasUpgrade(upgrade)
   do return true end
   return self.unit.player.deck[self.unit.class.code].upgrades[self.code][upgrade]
+end
+
+function Ability:canUse()
+  return self.timer == 0
+end
+
+function Ability:rot()
+  self.timer = timer.rot(self.timer, function()
+    f.exe(self.ready, self)
+  end)
+end
+
+function Ability:used()
+  self.timer = self.cooldown or 0
 end

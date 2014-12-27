9afae3c4da5874e3e9e89bf51e236041480cfb07
bjorn <tie.372@gmail.com>
2014-12-26 23:37:26 -0700

    opps;

--- app/netclient.lua
+++ app/netclient.lua
@@ -99,7 +99,7 @@ NetClient.messages.snapshot = {
   receive = function(self, event)
     if self.state ~= 'playing' then return end
 
-    print(tick - data.tick)
+    print(tick - event.data.tick)
 
     table.each(event.data.players, function(data)
       local p = ctx.players:get(data.id)

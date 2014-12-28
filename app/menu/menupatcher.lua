133a71070bc2c4396b660cbc13da9b356b8c5df4
bjorn <tie.372@gmail.com>
2014-12-28 02:21:40 -0700

    Fix patcher;

--- app/menu/menupatcher.lua
+++ app/menu/menupatcher.lua
@@ -20,14 +20,22 @@ function MenuPatcher:hubMessage(message, data)
 end
 
 function MenuPatcher:patch()
+  local ignore = {'.git', '.DS_Store', 'error.log'}
   local function halp(base)
-    if not base:find('.git') then
-      for _, file in ipairs(love.filesystem.getDirectoryItems(base)) do
-        local path = base .. '/' .. file
+    for _, file in ipairs(love.filesystem.getDirectoryItems(base)) do
+      local path = base .. '/' .. file
+
+      local ignored = false
+
+      table.each(ignore, function(name)
+        if path:find(name) then ignored = true return false end
+      end)
+
+      if not ignored then
         if love.filesystem.isDirectory(path) then
           halp(path)
         else
-          table.insert(self.hashes, {path = path, hash = sha1(path)})
+          table.insert(self.hashes, {path = path, hash = sha1(love.filesystem.read(path))})
         end
       end
     end
@@ -36,7 +44,7 @@ function MenuPatcher:patch()
   halp('')
 
   table.sort(self.hashes, function(a, b) return a.path < b.path end)
-  self.hashes = table.map(self.hashes, function(entry) return entry.has end)
+  self.hashes = table.map(self.hashes, function(entry) return entry.hash end)
 
   local patchHash = sha1(table.concat(self.hashes, ''))
 
